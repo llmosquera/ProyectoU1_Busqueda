3ef6674448617ef698d9539269c22dca75e552de
llmosquera <lmmosquera@espe.edu.ec>
2022-06-09 17:17:34 -0500

    definimos el for para poder registrar las zonas

--- ProgramaBFSS.py
+++ ProgramaBFSS.py
@@ -10,5 +10,15 @@ class Grafo:
         self.nombreNodos=[]
         #Establece la posición actual del grafo
         self.raiz=inPosicionInicial
+         #Se va a registrar el nomrbe de los diferentes nodos
+    def registrarZonas(self):
+        #recorremos para agregar nombres al array de nombre nodos
+        for nodo in range(0,self.numeroNodos,+1):
+            nombre= input('Ingrese el nombre del nodo: '+str(nodo)+': ')
+            self.nombreNodos.append(nombre)
+        self.estructurarNodos()
+        
+    #Muestra el grafo completo            
+    def mostrarZonas(self):
     
    
